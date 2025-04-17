06130dc853bc53677a92dec9a6ea63761e1c7d63
MaxTrod <maximsiminko@gmail.com>
2025-04-17 23:13:57 +0500

    починили анимации при загрузке

--- src/js/components/animate.js
+++ src/js/components/animate.js
@@ -1,8 +1,14 @@
 // Подключение анимаций по скроллу
 import AOS from 'aos';
-AOS.init({
-  disable: function () {
-    var maxWidth = 767.98;
-    return window.innerWidth < maxWidth;
+
+
+document.onreadystatechange = function () {
+  if (document.readyState == "complete") {
+    AOS.init({
+      disable: function () {
+        var maxWidth = 767.98;
+        return window.innerWidth < maxWidth;
+      }
+    });
   }
-});
+};
